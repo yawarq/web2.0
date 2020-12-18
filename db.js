e6d2bfffc37d90fd5ffce19457463056c62e191f
yawarq6@gmail.com <yawarq6@gmail.com>
2020-12-19 00:44:22 +0530

    mysql

---
+++ db.js
@@ -0,0 +1,31 @@
+var mysql = require('mysql');
+var db_config = {
+    host: '127.0.0.1',
+    user:'root',
+    password:'yawarking2000',
+    database:'webapp'
+}
+
+var connection;
+
+
+function handleDisconnect(){
+    connection = mysql.createConnection(db_config);
+    connection.connect(function(err){
+        if(err){
+            console.log('error when connectiont to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+    connection.on('error', function(err){
+        if(err.code==='PROTOCOL_CONNECTION_LOST'){
+            handleDisconnect();
+        }else{
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
+
+module.exports= connection;
