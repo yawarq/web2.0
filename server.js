ec2a932eab913ecfc06b51c1bda0b4da8dcd419a
yawarq6@gmail.com <yawarq6@gmail.com>
2020-12-19 23:47:56 +0530

    query

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const {save_user_information} = require('./models/server_db');
 
 app.use(bodyParser.json());
 
 
-app.post('/',(req,res)=>{
+app.post('/',async (req,res)=>{
     var email = req.body.email;
     var amount = req.body.amount;
    
@@ -18,7 +18,8 @@ app.post('/',(req,res)=>{
         return res.send(return_info);
     }
     else{
-        res.send({"amount":amount, "email":email});
+        var result = await save_user_information({"amount":amount, "email":email});
+        res.send(result);
     }
 
 });
