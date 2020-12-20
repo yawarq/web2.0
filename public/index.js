db08708d53c8f9dac4b3d221301ae064a34e4b74
yawarq6@gmail.com <yawarq6@gmail.com>
2020-12-20 13:14:36 +0530

    adding fields

---
+++ public/index.js
@@ -0,0 +1,30 @@
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            'total_amount':1000
+        }
+    }
+    render(){
+        return(
+          <div>
+              <h1>LOTTERY INFORMATION</h1>
+              <div>
+                  <p>Total lottery amount is {this.state.total_amount}</p>
+              </div>
+              <form>
+                  <input placeholder="amount" />
+                  <input placeholder="email"/>
+                  <button>Participate</button>
+              </form>
+          </div>
+        )
+    }
+};
+
+ReactDOM.render(
+    <div>
+        <App />
+    </div>
+    , document.getElementById('reactBinding')
+);
