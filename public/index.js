3a7ddc8a110b08f27a4551a60c4989d27cbf9349
yawarq6@gmail.com <yawarq6@gmail.com>
2020-12-20 23:26:48 +0530

    getting back

--- public/index.js
+++ public/index.js
@@ -8,6 +8,12 @@ class App extends React.Component{
             'email':'' ,
         }
     }
+
+    async componentDidMount(){
+        const result = await axios.get('/get_total_amount');
+    
+        this.setState({total_amount:result.data["0"].total_amount})
+    }
      
     onSubmit = async(event)=>{
         event.preventDefault();
